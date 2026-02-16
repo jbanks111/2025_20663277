46077350067c4362732266ba78e2a6aa07ce56f2
jbanks111 <eeyjb10@nottingham.ac.uk>
2026-02-16 10:13:15 +0000

    added the add button to display qmessage box

--- worksheet6/worksheet6task1/task1widgets/mainwindow.cpp
+++ worksheet6/worksheet6task1/task1widgets/mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton, &QPushButton::released, this, &MainWindow::handleButton);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::handleButton()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Add button was clicked");
+    msgBox.exec();
 }
