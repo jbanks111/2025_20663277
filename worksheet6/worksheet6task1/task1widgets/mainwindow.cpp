5608fd97439f81c12199eea6259098952f8301b1
jbanks111 <eeyjb10@nottingham.ac.uk>
2026-03-01 16:51:46 +0000

    adding treeview demo code to main window

--- worksheet6/worksheet6task1/task1widgets/mainwindow.cpp
+++ worksheet6/worksheet6task1/task1widgets/mainwindow.cpp
@@ -12,6 +12,41 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton_2, &QPushButton::clicked, this, &MainWindow::handleSubtractButton);
 
     connect(this, &MainWindow::statusUpdateMessage, ui->statusbar, &QStatusBar::showMessage);
+    /* Create/allocate the ModelList */
+    this->partList = new ModelPartList("PartsList");
+
+    /* Link it to the tree view in the GUI */
+    ui->treeView->setModel(this->partList);
+
+    /* Manually create a model tree = there are much better and more flexible ways of doing this,
+       e.g. with nested functions. This is just a quick example as a starting point. */
+    ModelPart* rootItem = this->partList->getRootItem();
+
+    /* Add 3 top level items */
+    for (int i = 0; i < 3; i++) {
+
+        /* Create strings for both data columns */
+        QString name = QString("TopLevel %1").arg(i);
+        QString visible("true");
+
+        /* Create child item */
+        ModelPart* childItem = new ModelPart({ name, visible });
+
+        /* Append to tree top-level */
+        rootItem->appendChild(childItem);
+
+        /* Add 5 sub-items */
+        for (int j = 0; j < 5; j++) {
+
+            QString name = QString("Item %1,%2").arg(i).arg(j);
+            QString visible("true");
+
+            ModelPart* childChildItem = new ModelPart({ name, visible });
+
+            /* Append to parent */
+            childItem->appendChild(childChildItem);
+        }
+    }
 }
 
 MainWindow::~MainWindow()
